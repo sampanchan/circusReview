3a3d706fab0446f019fa67c22dfc4e2a88e39228
Christopher Silich <chris.silich@gmail.com>
2021-09-02 17:50:01 -0400

    program and cohort pickers working, ux questionable

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,63 @@
 "use strict";
 
-console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
-document.querySelectorAll('.big-letter').forEach(function (letter) {
+// Navigation
+var pickedCohort = 'all';
+var pickedProgram = 'all';
+var allStudents = document.querySelectorAll('.student');
+var cohortPickerItems = document.querySelectorAll('.cohort-picker li');
+var programPickerItems = document.querySelectorAll('.program-picker li');
+cohortPickerItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    var currentCohortItem = document.querySelector('.cohort-picker .active');
+    if (currentCohortItem === this) return;
+    currentCohortItem.classList.remove('active');
+    this.classList.add('active');
+    console.log('pickedCohort', pickedCohort);
+    pickedCohort = this.dataset.cohort;
+    showHideStudents();
+  });
+});
+programPickerItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    var currentProgramItem = document.querySelector('.program-picker .active');
+    if (currentProgramItem === this) return;
+    currentProgramItem.classList.remove('active');
+    this.classList.add('active');
+    console.log('pickedProgram', pickedProgram);
+    pickedProgram = this.dataset.program;
+    showHideStudents();
+  });
+});
+
+var showHideStudents = function showHideStudents() {
+  allStudents.forEach(function (student) {
+    if ((student.dataset.program === pickedProgram || 'all' === pickedProgram) && (student.dataset.cohort === pickedCohort || 'all' === pickedCohort)) {
+      // show!
+      student.style.display = 'flex';
+    } else {
+      // hide!
+      student.style.display = 'none';
+    }
+  });
+}; // Silly animations up top
+
+
+var bigLetters = document.querySelectorAll('.big-letter');
+bigLetters.forEach(function (letter) {
   letter.addEventListener('mouseenter', function () {
+    if (letter.classList.contains('animate')) return;
     letter.classList.add('animate');
     setTimeout(function () {
       letter.classList.remove('animate');
     }, 1000);
   });
 });
+setInterval(function () {
+  var letter = bigLetters[Math.floor(Math.random() * bigLetters.length)];
+  if (letter.classList.contains('animate')) return;
+  letter.classList.add('animate');
+  setTimeout(function () {
+    letter.classList.remove('animate');
+  }, 1000);
+}, 3000);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,63 @@
-console.log(` 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+
+// Navigation
+let pickedCohort = 'all'
+let pickedProgram = 'all'
+let allStudents = document.querySelectorAll('.student')
+let cohortPickerItems = document.querySelectorAll('.cohort-picker li')
+let programPickerItems = document.querySelectorAll('.program-picker li')
+cohortPickerItems.forEach(item => {
+	item.addEventListener('click', function() {
+		let currentCohortItem = document.querySelector('.cohort-picker .active')
+		if (currentCohortItem === this) return
+		currentCohortItem.classList.remove('active')
+		this.classList.add('active')
+		console.log('pickedCohort',pickedCohort)
+		pickedCohort = this.dataset.cohort
+		showHideStudents()
+	})
+})
+programPickerItems.forEach(item => {
+	item.addEventListener('click', function() {
+		let currentProgramItem = document.querySelector('.program-picker .active')
+		if (currentProgramItem === this) return
+		currentProgramItem.classList.remove('active')
+		this.classList.add('active')
+		console.log('pickedProgram',pickedProgram)
+		pickedProgram = this.dataset.program
+		showHideStudents()
+	})
+})
+let showHideStudents = () => {
+	allStudents.forEach(student => {
+		if ((student.dataset.program === pickedProgram || 'all' === pickedProgram) && (student.dataset.cohort === pickedCohort || 'all' === pickedCohort) ) {
+			// show!
+			student.style.display = 'flex'
+		} else {
+			// hide!
+			student.style.display = 'none'
+		}
+	})
+}
 
 
-document.querySelectorAll('.big-letter').forEach(letter => {
+// Silly animations up top
+let bigLetters = document.querySelectorAll('.big-letter')
+
+bigLetters.forEach(letter => {
 	letter.addEventListener('mouseenter', () => {
+		if (letter.classList.contains('animate')) return
 		letter.classList.add('animate')
 		setTimeout(() => {
 			letter.classList.remove('animate')
 		}, 1000)
 	})
 })
+
+setInterval(() => {
+	let letter = bigLetters[Math.floor(Math.random() * bigLetters.length)]
+	if (letter.classList.contains('animate')) return
+	letter.classList.add('animate')
+	setTimeout(() => {
+		letter.classList.remove('animate')
+	}, 1000)
+}, 3000)
