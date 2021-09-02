4d3683285ccbfc7304583568281f5441bbbf6927
Christopher Silich <chris.silich@gmail.com>
2021-09-01 22:09:03 -0400

    silly animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,12 @@
 "use strict";
 
 console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+document.querySelectorAll('.big-letter').forEach(function (letter) {
+  letter.addEventListener('mouseenter', function () {
+    letter.classList.add('animate');
+    setTimeout(function () {
+      letter.classList.remove('animate');
+    }, 1000);
+  });
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 console.log(` 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+
+document.querySelectorAll('.big-letter').forEach(letter => {
+	letter.addEventListener('mouseenter', () => {
+		letter.classList.add('animate')
+		setTimeout(() => {
+			letter.classList.remove('animate')
+		}, 1000)
+	})
+})
